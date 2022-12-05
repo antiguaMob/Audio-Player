69d3e174a658e39c93b735bc494d3b3049dc7a44
antiguaMob <antigua_m@mail.ru>
2022-12-06 02:36:08 +0300

    lesson 4

--- app/src/main/java/com/antigua/myaudioplayer/media/constants/K.kt
+++ app/src/main/java/com/antigua/myaudioplayer/media/constants/K.kt
@@ -2,7 +2,7 @@ package com.antigua.myaudioplayer.media.constants
 
 object K {
     const val PLAYBACK_NOTIFICATION_CHANNEL_ID = "sdbdewuifdhefgoo"
-    const val PLAYBACK_NOTIFICATION_ID = 126776235336
+    const val PLAYBACK_NOTIFICATION_ID = 1267
     const val MEDIA_ROOT_ID = "ffgfgrjiogioigh"
     const val START_MEDIA_PLAY_ACTION = "START_MEDIA_PLAY_ACTION"
     const val REFRESH_MEDIA_PLAY_ACTION = "REFRESH_MEDIA_PLAY_ACTION"
